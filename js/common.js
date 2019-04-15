17a20a2e2a14c392dc430d91ae1d689d5532a84e
Valeriiyukov <valeryuyukov@gmail.com>
2019-04-15 20:47:42 +0300

    add adaptive and slider to spectacle page

--- js/common.js
+++ js/common.js
@@ -26,13 +26,6 @@ $('.backward-arrow').on('click', function(){
     history.back(-1);
 })
     
-    var scrollorama = $.scrollorama({
-        blocks:'.scrollblock'
-    });
-    
-    scrollorama.animate('.preview',{
-    duration:400, property:'opacity'
-})
 
 });
 
@@ -46,5 +39,45 @@ $('.backward-arrow').on('click', function(){
 //   if (scroll >= stickyOffset) sticky.addClass('fixed');
 //   else sticky.removeClass('fixed');
 //   });
+
+
+$('.gallery__slider, .video-slider__slider').slick({
+  infinite: true,
+  arrows: false,
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+          cssEase: 'linear',
+    variableWidth: true,
+    variableHeight: true,
+        infinite: true,
+          arrows: false
+      }
+    }
+  ]
+});
+
+$('.gallery-slider__btn-next').on('click', function() {
+  $('.gallery__slider').slick('slickNext');
+});
+
+$('.gallery-slider__btn-prev').on('click', function() {
+  $('.gallery__slider').slick('slickPrev');
+});
+
+$('.video-slider__btn-next').on('click', function() {
+  $('.video-slider__slider').slick('slickNext');
+});
+
+$('.video-slider__btn-prev').on('click', function() {
+  $('.video-slider__slider').slick('slickPrev');
+});
+
+
 
 
